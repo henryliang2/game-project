374fb07a897af34366f65d3d870c59e1bc1459ce
zomgitshenry <henryliang@alumni.ubc.ca>
2020-11-05 12:53:55 -0800

    initialized Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../styles/App.css'
+import './../styles/Footer.css'
+
+const Footer = () => {
+
+  return (
+    <div className='footer'>
+      Hello Footer
+    </div>
+  );
+}
+
+export default Footer;
